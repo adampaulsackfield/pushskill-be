4c526e2446d8d9145c97c8a2e19e332dcd7a672f
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-03 16:38:01 +0100

    fixed spacing

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,65 +1,77 @@
-const User = require("../models/user.model.js");
-const hashPassword = require("../utlis/hashPassword");
-const comparePassword = require("../utlis/comparePassword");
-const generateToken = require("../utlis/generateToken");
+const User = require('../models/user.model.js');
+const hashPassword = require('../utlis/hashPassword');
+const comparePassword = require('../utlis/comparePassword');
+const generateToken = require('../utlis/generateToken');
 
 exports.getUsers = async (req, res, next) => {
-  const users = await User.find();
+	const users = await User.find();
 
-  res.status(200).send(users);
+	res.status(200).send(users);
 };
 
 exports.postUser = async (req, res, next) => {
-  const { username, password } = req.body;
-  try {
-    if (!username || !password) {
-      throw new Error("missing required field");
-    }
-    const userExists = await User.findOne({ username });
+	const { username, password } = req.body;
 
-    if (userExists) {
-      throw new Error("user already exists");
-    }
-    let newUser = {
-      username,
-      password: await hashPassword(password),
-    };
-    const user = await User.create(newUser);
-    res.status(201).send(user);
-  } catch (error) {
-    if (error.message) {
-      res.status(400).send({ message: error.message });
-    } else {
-      res.status(500).send({ message: "server error" });
-    }
-  }
+	try {
+		if (!username || !password) {
+			throw new Error('missing required field');
+		}
+
+		const userExists = await User.findOne({ username });
+
+		if (userExists) {
+			throw new Error('user already exists');
+		}
+
+		let newUser = {
+			username,
+			password: await hashPassword(password),
+		};
+
+		const user = await User.create(newUser);
+
+		res.status(201).send(user);
+	} catch (error) {
+		if (error.message) {
+			res.status(400).send({ message: error.message });
+		} else {
+			res.status(500).send({ message: 'server error' });
+		}
+	}
 };
 
 exports.loginUser = async (req, res, next) => {
-  const { username, password } = req.body;
-  try {
-    if (!username || !password) {
-      throw new Error("missing required field");
-    }
-    const user = await User.findOne({ username });
-    if (!user) {
-      throw new Error("invalid login credentials");
-    }
-    const passwordMatch = await comparePassword(password, user.password);
-    if (!passwordMatch) {
-      throw new Error("invalid login credentials");
-    }
-    let validatedUser = {
-      username: user.username,
-      id: user.id,
-      token: generateToken(user.id),
-    };
-    res.status(200).send(validatedUser);
-  } catch (error) {
-    if (error.message) {
-      res.status(400).send({ message: error.message });
-    } else {
-      res.status(500).send({ message: "server error" });
-    }
-  }
+	const { username, password } = req.body;
+
+	try {
+		if (!username || !password) {
+			throw new Error('missing required field');
+		}
+
+		const user = await User.findOne({ username });
+
+		if (!user) {
+			throw new Error('invalid login credentials');
+		}
+
+		const passwordMatch = await comparePassword(password, user.password);
+
+		if (!passwordMatch) {
+			throw new Error('invalid login credentials');
+		}
+
+		let validatedUser = {
+			username: user.username,
+			id: user.id,
+			token: generateToken(user.id),
+		};
+
+		res.status(200).send(validatedUser);
+	} catch (error) {
+		if (error.message) {
+			res.status(400).send({ message: error.message });
+		} else {
+			res.status(500).send({ message: 'server error' });
+		}
+	}
 };
