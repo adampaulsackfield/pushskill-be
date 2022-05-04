45386c367c9d46e0ea88bf8cc608cc70cf5f20f6
Dane <danejwhitfield@gmail.com>
2022-05-04 12:44:59 +0100

    GET/POST room_id/messages

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -2,6 +2,7 @@ const {
 	getRooms,
 	createRoom,
 	getRoom,
+	getRoomMessages,
 } = require('../controllers/room.controller');
 const protectedRoute = require('../middleware/authMiddleware');
 const roomRouter = require('express').Router();
@@ -12,5 +13,7 @@ roomRouter
 	.post(protectedRoute, createRoom);
 
 roomRouter.route('/:room_id').get(protectedRoute, getRoom);
+
+roomRouter.route('/:room_id/messages').get(protectedRoute, getRoomMessages);
 
 module.exports = roomRouter;
