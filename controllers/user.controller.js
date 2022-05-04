859995b0d3b2765da35bde83c4347963b924d98e
Adam Sackfield <adamsackfield20@gmail.com>
2022-05-04 17:34:32 +0100

    fix conflicts from merge

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -94,7 +94,8 @@ const patchUserAchievements = async (req, res) => {
 
 		const user = await User.findByIdAndUpdate(
 			{ _id: user_id },
-			{ $push: { achievements: achievement } }
+			{ $push: { achievements: achievement } },
+			{ new: true }
 		);
 
 		if (!user) {
