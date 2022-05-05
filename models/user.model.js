cb753c9fdcdc3a537bf95a53633fbe6a458e622f
Dane <danejwhitfield@gmail.com>
2022-05-05 20:37:28 +0100

    updated default user avatar

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const achievementSchema = new mongoose.Schema(
 		url: {
 			type: 'string',
 			default:
-				'https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png',
+				'https://i.picsum.photos/id/17/200/200.jpg?hmac=9QDzoqdXorZialFww894D6BqJGalCXFLX2zNQtYENEA',
 		},
 		description: 'string',
 	},
@@ -17,7 +17,11 @@ const userSchema = mongoose.Schema({
 	username: { unique: true, required: true, type: String },
 	firstName: { type: String },
 	lastName: { type: String },
-	avatarUrl: { type: String },
+	avatarUrl: {
+		type: String,
+		default:
+			'https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png',
+	},
 	goal: { type: String },
 	traits: { default: [], type: [String] },
 	learningInterests: { type: [String], default: [] },
